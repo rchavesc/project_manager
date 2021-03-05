0476f34df26499d7b3686fb3dc3e3d4a04970244
Randall Chaves Camacho <rchavesc1@gmail.com>
2021-03-04 19:47:24 -0600

    complete

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,41 @@
+class ProjectsController < ApplicationController
+
+  http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :index
+
+  def index
+    @projects = Project.all
+    if params[:status] and params[:status] != 'all'
+      @projects = @projects.where(status: params[:status])
+    end
+  end
+
+  def new
+    @project = Project.new
+  end
+
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to root_path
+    else
+      render 'new'
+    end 
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    if @project.destroy
+      redirect_to root_path
+    end
+  end
+
+
+
+
+  private
+  #strong params
+  def project_params
+    params.require(:project).permit(:name, :description, :started_at, :ended_at, :status)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'projects#index'
+  get 'dashboard', to: 'project#index'
+  get 'projects/new', to: 'projects#new'
+  post 'projects', to: 'projects#create'
+  delete 'projects/:id', to: 'projects#destroy', as: 'destroy_project'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
